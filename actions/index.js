8503317237771e63e114dac93c17a057d5b9a64a
msosborne3 <msosborne@ymail.com>
2017-04-05 17:37:48 -0500

    Done.

--- actions/index.js
+++ actions/index.js
@@ -1,4 +1,14 @@
+const counterStore = require('../stores/counterStore');
+
+const increment = () => {
+  counterStore.increment();
+};
+
+const decrement = () => {
+  counterStore.decrement();
+};
+
 module.exports = {
-  // Your implementation here.
-  // Hint: We need two actions!
+  increment,
+  decrement
 };
--- stores/counterStore.js
+++ stores/counterStore.js
@@ -1,6 +1,21 @@
-class CounterStore {
+const Store = require('./Store');
+
+class CounterStore extends Store {
   // Your implementation here.
   // Hint: extend the Store class!
+  constructor() {
+    super(0);
+  }
+
+  increment() {
+    const nextState = this.getState() + 1;
+    this.setState(nextState);
+  }
+
+  decrement() {
+    const nextState = this.getState() - 1;
+    this.setState(nextState);
+  }
 }
 
 module.exports = new CounterStore();
